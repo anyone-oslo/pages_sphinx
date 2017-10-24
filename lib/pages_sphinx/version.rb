fec39db008af9d62a15355379c4b8dfdc271bc3c
Inge Jørgensen <inge@anyone.no>
2017-10-24 14:13:07 +0200

    Version 0.0.3

--- lib/pages_sphinx/version.rb
+++ lib/pages_sphinx/version.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
 
 module PagesSphinx
-  VERSION = "0.0.2".freeze unless PagesSphinx.const_defined?("VERSION")
+  VERSION = "0.0.3".freeze unless PagesSphinx.const_defined?("VERSION")
 end
